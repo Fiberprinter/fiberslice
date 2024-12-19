8b71ec20a76c9b4d272b53b53d8ac51fc98490fd
Elias Gottsbacher <eli.gottsbacher@gmail.com>
2024-12-19 23:53:24 +0100

    dispatch moves calculated distances

--- slicer/src/dispatcher.rs
+++ slicer/src/dispatcher.rs
@@ -1,11 +1,19 @@
+use geo::EuclideanDistance;
+
 use crate::{MoveChain, Settings};
 
 pub fn dispatch_moves(chains: &mut Vec<MoveChain>, settings: &Settings) {
+    let mut traces = 0.0;
+
     for chain in chains.iter_mut() {
+        let start = chain.start_point;
+
         for mov in chain.moves.iter_mut() {
+            let distance = mov.end.euclidean_distance(&start);
+
             match mov.move_type {
-                crate::MoveType::WithFiber(move_print_type) => todo!(),
-                crate::MoveType::WithoutFiber(move_print_type) => todo!(),
+                crate::MoveType::WithFiber(move_print_type) => {}
+                crate::MoveType::WithoutFiber(move_print_type) => traces += distance,
                 crate::MoveType::Travel => todo!(),
             }
         }
